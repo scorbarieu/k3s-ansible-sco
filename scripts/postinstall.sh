e7579c19d70b82a18dc39a55f738311c87c3dd4f
Stephane Corbarieu <scorbarieu@hotmail.com>
2022-10-21 16:34:35 +0000

    updated postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,13 +4,35 @@ CERTMANAGER_VERSION=v1.7.1
 ADMINPASSWORD=TestMeteo#2022
 LONGHORNUIUSER=longhorn; 
 LONGHORNUIPASSWORD=$ADMINPASSWORD;
+#ADMINUSER=vagrant #requires sudo
+CLUSTERADMINUSER=corbarieus
+CLUSTERMASTER=nifif31-sidev.meteo.fr
 
+
+
+if [ ! -f /usr/local/bin/helm ]; then
+    echo "installing helm"
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    sudo dnf install bash-completion
+    helm completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+fi
+if [ ! -f /usr/local/bin/kubectl ]; then
+    echo "install kubectl"
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.25.0/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+    mkdir ~/.kube
+    sudo dnf install bash-completion
+    kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+fi
+
+if [ ! -f ~/.kube/config ]; then
+    echo "install kubectl config"
+    scp $CLUSTERADMINUSER@$CLUSTERMASTER:~/.kube/config ~/.kube/config
+fi
 
 echo "checking nodes"
 kubectl get nodes
-
-echo "installing helm"
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 echo "Install certmanager"
 helm repo add jetstack https://charts.jetstack.io
@@ -23,15 +45,20 @@ kubectl create namespace cattle-system
 helm install rancher rancher-stable/rancher   --namespace cattle-system   --set hostname=rancher.my.org   --set bootstrapPassword=$ADMINPASSWORD
 
 echo "installing longhorn block storage for PV"
-sudo dnf -y  install iscsi-initiator-utils
-sudo dnf install nfs-utils -y
-kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
-#then set up the UI access
-echo "${LONGHORNUIUSER}:$(openssl passwd -stdin -apr1 <<< ${LONGHORNUIPASSWORD})" >> auth
-kubectl -n longhorn-system create secret generic basic-auth --from-file=auth
-kubectl -n longhorn-system apply -f appsamples/longhorningress.yaml
-# EXAMPLE create a storage class
-kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/master/examples/storageclass.yaml
-# EXAMPLE create a PV and pod (not WORKING on vagrant rocky8...yet... volome created but failed to attach to container)
-# For some reason the volume fails to attach
-kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/master/examples/pod_with_pvc.yaml
+echo "checking longhorn pre-requisites"
+sudo dnf install jq -y # required by the script
+curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.3.1/scripts/environment_check.sh | bash
+if [ $? == 0 ]; then
+    kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
+    #then set up the UI access
+    echo "${LONGHORNUIUSER}:$(openssl passwd -stdin -apr1 <<< ${LONGHORNUIPASSWORD})" >> auth
+    kubectl -n longhorn-system create secret generic basic-auth --from-file=auth
+    kubectl -n longhorn-system apply -f appsamples/longhorningress.yaml
+    # EXAMPLE create a storage class
+    kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/master/examples/storageclass.yaml
+    # EXAMPLE create a PV and pod (not WORKING on vagrant rocky8...yet... volome created but failed to attach to container)
+    # For some reason the volume fails to attach
+    kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/master/examples/pod_with_pvc.yaml
+else
+    echo "be sure pre-requistes for longhorn have been installed...skipping"
+fi
